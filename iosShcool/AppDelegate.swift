6dee6e7be8df34cc810f077046f128a6ccf4f023
Ksenya Kulizhnikova <k_latushkina@mail.ru>
2023-04-24 21:11:26 +0700

    lesson 13

--- iosShcool/AppDelegate.swift
+++ iosShcool/AppDelegate.swift
@@ -12,4 +12,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   internal lazy var assembly = Assembly()
 
+  func application(
+          _ application: UIApplication,
+          didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+      ) -> Bool {
+        assembly.storageManager.cleanKeychainIfNeeded()
+          return true
+      }
+
 }
